5e12bce0cf0f927da20b516feaa944b09ed6b4f5
GerasiMandMumu <konst.02@yandex.ru>
2022-10-22 19:46:42 +0300

    Правки

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Response, Body, Form
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi import FastAPI, Response, Path, Body
+from fastapi.responses import JSONResponse, FileResponse, PlainTextResponse
 from fastapi.encoders import jsonable_encoder
+import traceback
 
-
-def calculate(phrase):
+def calculate(phrase: str) -> str:
     loc = {}
     exec(f'result = ({phrase})', globals(), loc)
     result = loc['result']
@@ -15,18 +15,38 @@ app = FastAPI()
 async def root():
     return FileResponse("index.html")
 
-@app.get("/eval/{phrase}")
-async def eval(phrase: str):
-    result = calculate(phrase)
-    data = {'Результат = ': result}
-    json_data = jsonable_encoder(data)
-    return JSONResponse(content=json_data, media_type="text/plain", status_code=200)
-
-
+@app.get("/eval/{phrase}", status_code=200)
+async def eval(response: Response, phrase: str = Path(min_length=3)):
+    try:
+        result = calculate(phrase)
+        text_data = 'Результат = ' + str(result)
+        response.status_code = 200
+    except BaseException:
+        response.status_code = 400
+        text_data = traceback.format_exc()
+    finally:
+        return PlainTextResponse(content=text_data)
+         
+    
 @app.post("/eval")
-async def eval(phrase: str = Form('test')):
-    result = calculate(phrase)
-    data = {'Результат = ': result}
-    json_data = jsonable_encoder(data)
-    return JSONResponse(content=json_data, media_type="text/plain", status_code=201)
+async def eval(data = Body()):
+    try:
+        phrase = data["phrase"]
+        result = calculate(phrase)
+        data = {'result': result}
+        json_data = jsonable_encoder(data)
+        response_status_code = 200
+    except BaseException:
+        response_status_code = 400
+        tb = traceback.format_exc()
+        data = {'result': tb}
+        json_data = jsonable_encoder(data)
+    finally:
+        return JSONResponse(content=json_data, media_type="application/json", status_code=response_status_code)    
+
+    
+    
+    
+
+    
 
