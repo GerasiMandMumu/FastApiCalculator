dc96fbf4ba19e0e0e42bcbdda9497e3e2d6d0d7d
GerasiMandMumu <konst.02@yandex.ru>
2022-10-22 17:00:03 +0300

    1. Исправление dockerfile, requirements и main. 2. Добавил __init__.py

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,7 @@ WORKDIR /code
 COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 COPY ./app /code/app
+
+COPY ./app/index.html /code/app/index.html
+
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ from fastapi.encoders import jsonable_encoder
 
 
 def calculate(phrase):
-    exec(f'result = ({phrase})')
+    loc = {}
+    exec(f'result = ({phrase})', globals(), loc)
+    result = loc['result']
     return result
    
 app = FastAPI()
@@ -12,20 +14,19 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return FileResponse("index.html")
-    #return {"message": "Hello, World!"}
 
 @app.get("/eval/{phrase}")
 async def eval(phrase: str):
-    result = phrase
-    print(result.split('+'))
+    result = calculate(phrase)
     data = {'Результат = ': result}
     json_data = jsonable_encoder(data)
     return JSONResponse(content=json_data, media_type="text/plain", status_code=200)
 
 
 @app.post("/eval")
-async def eval(phrase: str = Form()):
-    data = {'Результат = ': phrase}
+async def eval(phrase: str = Form('test')):
+    result = calculate(phrase)
+    data = {'Результат = ': result}
     json_data = jsonable_encoder(data)
     return JSONResponse(content=json_data, media_type="text/plain", status_code=201)
 
