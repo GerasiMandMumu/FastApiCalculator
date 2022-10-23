11e01791cc4d6489976f5ffccfcb053473a3c36f
GerasiMandMumu <konst.02@yandex.ru>
2022-10-23 21:36:51 +0300

    Убрал лишнее

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Response, Path, Body, Query
+from fastapi import FastAPI, Body, Query
 from fastapi.responses import JSONResponse, FileResponse, PlainTextResponse
 from fastapi.encoders import jsonable_encoder
 import traceback
 
+# вычисление выражения
 def calculate(phrase: str) -> str:
     loc = {}
     exec(f'result = ({phrase})', globals(), loc)
@@ -11,14 +12,20 @@ def calculate(phrase: str) -> str:
    
 app = FastAPI()
 
+
+# старт приложения
 @app.get("/")
 async def root():
     return FileResponse("app/index.html")
 
+
+# GET-запрос
 @app.get("/eval")
 async def eval(phrase: str = Query(default=None, min_length=3)):
+    
+    # если выражение вычисляется, то выводится результат, иначе, сообщение об ошибке
+    
     try:
-        print(phrase)
         result = calculate(phrase)
         text_data = 'Результат = ' + str(result)
         response_status_code = 200
@@ -27,18 +34,24 @@ async def eval(phrase: str = Query(default=None, min_length=3)):
         text_data = traceback.format_exc()
     finally:
         return PlainTextResponse(content=text_data, status_code=response_status_code)
+
          
-    
+# POST-запрос
 @app.post("/eval")
 async def eval(data = Body(default=None)):
+    
+    # если выражение вычисляется, то выводится результат, иначе, сообщение об ошибке
+    
     try:
         phrase = data["phrase"]
         result = calculate(phrase)
         data = {'result': result}
         json_data = jsonable_encoder(data)
-        response_status_code = 200
+        response_status_code = 201
     except BaseException:
         response_status_code = 400
+
+        # получения описания ошибки
         tb = traceback.format_exc()
         data = {'result': tb}
         json_data = jsonable_encoder(data)
